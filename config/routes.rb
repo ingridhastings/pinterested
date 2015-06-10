16a9faf03f4cb56aeb35f3ed041fde4d56dc0d4c
Ingrid Hastings <ingridhastings@gmail.com>
2015-06-10 18:38:09 -0500

    Added view files and update routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get 'home/about'
+  get 'home/contact'
+  get 'home/faq'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
