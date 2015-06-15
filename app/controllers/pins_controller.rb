a02c2f51c5d09ec47640ff4b60910b5e037eb921
Ingrid Hastings <ingridhastings@gmail.com>
2015-06-15 07:45:32 -0500

    Added user name, updated devise forms

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -54,7 +54,7 @@ class PinsController < ApplicationController
     end
 
     def pin_params
-      params.require(:pin).permit(:description, :image)
+      params.require(:pin).permit(:description, :image, :name)
     end
 
     def correct_user
