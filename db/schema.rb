ea9449cd128a4c615cd6501a715c191abd22c6c7
Ingrid Hastings <ingridhastings@gmail.com>
2015-07-06 13:33:40 -0500

    Add Devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612180902) do
+ActiveRecord::Schema.define(version: 20150621164648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "models", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "models", ["email"], name: "index_models_on_email", unique: true, using: :btree
+  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -28,6 +46,17 @@ ActiveRecord::Schema.define(version: 20150612180902) do
   end
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id", using: :btree
+
+  create_table "userrails", force: :cascade do |t|
+    t.string   "g"
+    t.string   "model"
+    t.string   "user"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
